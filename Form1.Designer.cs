bfe01c5b18074edefa09021cc94b2e5c8d586e61
FlyFurki <59234147+FlyFurki@users.noreply.github.com>
2021-05-13 19:34:57 +0400

    ok

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace passwordGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 6);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 19);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Low";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 31);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(65, 19);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Normal";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 56);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(60, 19);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Strong";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(12, 78);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(85, 19);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Very Strong";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(115, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 88);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 103);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(97, 23);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.ForestGreen;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(116, 104);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(44, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Copy";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Firebrick;
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(166, 104);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(48, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(217, 129);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Password Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace passwordGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked==true)
+            {
+                Random r = new Random();
+
+                char[] small1s = "qrtypsdfjklmnbvcxzghw".ToCharArray();
+                string small1String = "";
+                for (int i = 0; i < 1; i++)
+                {
+                    small1String += small1s[r.Next(0, 21)].ToString();
+                }
+
+                char[] small2s = "euioa".ToCharArray();
+                string small2String = "";
+                for (int i = 0; i < 1; i++)
+                {
+                    small2String += small2s[r.Next(0, 5)].ToString();
+                }
+
+                char[] small11s = "qrtypsdfjklmnbvcxzghw".ToCharArray();
+                string small11String = "";
+                for (int i = 0; i < 1; i++)
+                {
+                    small11String += small11s[r.Next(0, 21)].ToString();
+                }
+
+                char[] small22s = "euioa".ToCharArray();
+                string small22String = "";
+                for (int i = 0; i < 1; i++)
+                {
+                    small22String += small22s[r.Next(0, 5)].ToString();
+                }
+
+                char[] numbers = "1234567890".ToCharArray();
+                string numberSS = "";
+                for (int i = 0; i < 4; i++)
+                {
+                    numberSS += numbers[r.Next(0, 10)].ToString();
+                }
+
+                textBox1.Text = (small11String + small22String + small1String + small2String + numberSS);
+
+            }
+            else if (radioButton2.Checked == true)
+            {
+
+                char[] bigs = "QRTYPSDFJKLMNBVCXZGHW".ToCharArray();
+                Random r = new Random();
+                string bigString = "";
+                for (int i = 0; i < 1; i++)
+                {
+                    bigString += bigs[r.Next(0, 21)].ToString();
+                }
+
+                char[] small1s = "qrtypsdfjklmnbvcxzghw".ToCharArray();
+                string small1String = "";
+                for (int i = 0; i < 1; i++)
+                {
+                    small1String += small1s[r.Next(0, 21)].ToString();
+                }
+
+                char[] small2s = "euioa".ToCharArray();
+                string small2String = "";
+                for (int i = 0; i < 1; i++)
+                {
+                    small2String += small2s[r.Next(0, 5)].ToString();
+                }
+
+                char[] small11s = "qrtypsdfjklmnbvcxzghw".ToCharArray();
+                string small11String = "";
+                for (int i = 0; i < 1; i++)
+                {
+                    small11String += small11s[r.Next(0, 21)].ToString();
+                }
+
+                char[] small22s = "euioa".ToCharArray();
+                string small22String = "";
+                for (int i = 0; i < 1; i++)
+                {
+                    small22String += small22s[r.Next(0, 5)].ToString();
+                }
+
+                char[] numbers = "1234567890".ToCharArray();
+                string numberSS = "";
+                for (int i = 0; i < 5; i++)
+                {
+                    numberSS += numbers[r.Next(0, 10)].ToString();
+                }
+                textBox1.Text = (bigString + small22String + small11String + small2String + small1String + numberSS);
+
+            }
+            else if (radioButton3.Checked == true)
+            {
+                char[] bigs = "QRTYPSDFJKLMNBVCXZGHWEUAIO".ToCharArray();
+                Random r = new Random();
+                string bigString = "";
+                for (int i = 0; i < 2; i++)
+                {
+                    bigString += bigs[r.Next(0, 26)].ToString();
+                }
+                char[] small1s = "qrtypsdfjklmnbvcxzghweuioa".ToCharArray();
+                string small1String = "";
+                for (int i = 0; i < 3; i++)
+                {
+                    small1String += small1s[r.Next(0, 26)].ToString();
+                }
+                char[] numbers = "1234567890".ToCharArray();
+                string numberSS = "";
+                for (int i = 0; i < 3; i++)
+                {
+                    numberSS += numbers[r.Next(0, 10)].ToString();
+                }
+                char[] symbols = "()!?".ToCharArray();
+                string symbolSS = "";
+                for (int i = 0; i < 2; i++)
+                {
+                    symbolSS += symbols[r.Next(0, 4)].ToString();
+                }
+
+                textBox1.Text = (bigString + small1String + numberSS + symbolSS);
+
+            }
+            else if (radioButton4.Checked == true)
+            {
+
+                char[] landn1 = "1234567890qwertyuiopasdfjklmnbvcxzQAZXSWEDCVFRTBNYUJMKIOLP()!?()!?()!?".ToCharArray();
+                Random r = new Random();
+                string random1String = "";
+                for (int i = 0; i <= 12; i++)
+                {
+                    random1String += landn1[r.Next(0, 70)].ToString();
+                }
+                textBox1.Text = (random1String);
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+    }
+}
